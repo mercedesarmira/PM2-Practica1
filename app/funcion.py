0833c0985a0d8f0e4de19d2cf32cd8723c0aac84
mercedesarmira <armiraatz001@gmail.com>
2025-03-01 22:24:46 -0600

    Función versión 2: Documentada y más eficiente

--- app/funcion.py
+++ app/funcion.py
@@ -1,5 +1,37 @@
-
-# Función para encontrar el número más grande de la lista
+# Función que procesa una lista de datos numéricos
 
 def encontrar_maximo(lista_de_numeros):
-    return max(lista_de_numeros)
+    """
+    Encuentra el máximo de una lista de números, es decir, devuelve el número más grande de la lista.
+
+    Parameters
+    ----------
+    lista_de_numeros : list[int|float]
+        Lista de datos numéricos
+    
+    Returns
+    -------
+    int o float
+        El número más grande de la lista. Si la lista está vacía, devuelve None.
+    
+    Raises
+    ------
+    ValueError
+        Indica si la lista tiene elementos no aceptables.
+
+    Examples
+    --------
+    >>> encontrar_maximo([3, 2, 5])
+    5
+    >>> encontrar_maximo([1, 1])
+    1
+    >>> encontrar_maximo([])
+    None
+    """
+    if not lista_de_numeros: 
+        return None
+    try:
+        return max(lista_de_numeros)
+    except TypeError:
+        raise ValueError("Todos los elementos de la lista deben ser números int o float")
+        
